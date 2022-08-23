edb227056136472e31ac7d307b303b6e2bb9f400
Nora Schinkel <ncschinkel@gmail.com>
2022-08-23 14:26:06 +0200

    Use the config for the KPIs

--- klimaatraadpleging/__init__.py
+++ klimaatraadpleging/__init__.py
@@ -4,13 +4,14 @@ from klimaatraadpleging.etm_session import ETMSession
 from klimaatraadpleging.basic_scenarios import BasicScenarios
 from klimaatraadpleging.result import Result
 from klimaatraadpleging.combiner import Combiner
+from klimaatraadpleging.config import Config
 
 
 def generate_json():
     result = Result(
         Combiner(
             pd.read_csv('config/input_mapping.csv', index_col=[0,1], comment='#'),
-            [{'key': 'costs_KPI', 'gquery': 'total_costs'}], # Replace this with KPI config
+            Config().kpis, # Replace this with KPI config
             BasicScenarios(
                 pd.read_csv('config/basic_scenarios.csv', index_col=0, comment='#'),
                 pd.read_csv('config/supply_and_savings.csv', index_col=0, comment='#')
